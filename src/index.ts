1b964aa17fde3475548891a4dbe3e4c264f25eec
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-25 22:51:51 +0530

    feat(): setup up express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello-world");
+import express, { Express } from "express";
+const app: Express = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send({ msg: "Hello" });
+});
+
+app.listen(PORT, () => {
+  console.log("listening on port " + PORT);
+});
