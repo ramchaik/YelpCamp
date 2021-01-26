61b90f07e74055a9b46cd494fe0f9fd23a61e5bf
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-26 19:26:51 +0530

    feat(): add data seeding script

---
+++ src/seed/index.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import { Campgroud } from "../models/campgroud";
+import { cities } from "./cities";
+import { descriptors, places } from "./seedHelpers";
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (err: any) => console.error.bind(console, "connection error: "));
+
+db.once("open", () => {
+  console.log("Database connected for Seeding!");
+});
+
+const getRandomIndex = (range: number): number =>
+  Math.floor(Math.random() * range);
+const sample = (arr: any): any => arr[getRandomIndex(arr.length)];
+
+const seedDB = async () => {
+  await Campgroud.deleteMany({});
+
+  for (let i = 0; i < 50; ++i) {
+    const camp = Campgroud.build({
+      title: `${sample(descriptors)} ${sample(places)}`,
+      location: `${cities[getRandomIndex(1000)].city}, ${
+        cities[getRandomIndex(1000)].state
+      }`,
+      price: "$10",
+      description: "Seed data",
+    });
+
+    await camp.save();
+  }
+};
+
+export const runSeed = () =>
+  seedDB().then(async () => {
+    await mongoose.connection.close();
+    console.log("Seeding completed!");
+  });
