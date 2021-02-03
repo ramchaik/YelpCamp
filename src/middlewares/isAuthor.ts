948abfb3e3c68622472dc47a1b9744b64debc670
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-04 00:18:09 +0530

    refactor: update flash error message

--- src/middlewares/isAuthor.ts
+++ src/middlewares/isAuthor.ts
@@ -11,7 +11,7 @@ export const isAuthor = async (
   const campground = await Campground.findById(id);
 
   if (!campground.author.equals(req.user._id)) {
-    req.flash("error", "You do not have permission to do that");
+    req.flash("error", "You do not have permission to do that!");
     return res.redirect(`/campgrounds/${id}`);
   }
 
