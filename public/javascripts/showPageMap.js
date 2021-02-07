d9f0a91bdd6f52d85cfd7facbe916b5ac8ef46f6
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-07 19:54:20 +0530

    feat(maps): add popup on map marker

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,7 +3,14 @@ const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
   center: campground.geometry.coordinates,
-  zoom: 9, // starting zoom
+  zoom: 10, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
+    )
+  )
+  .addTo(map);
