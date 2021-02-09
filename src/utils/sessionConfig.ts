80815a326064f69bfcce1ae80a34383f9fb6dd2e
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-09 23:00:42 +0530

    feat(security): update session config

--- src/utils/sessionConfig.ts
+++ src/utils/sessionConfig.ts
@@ -1,14 +1,17 @@
 import { SessionOptions } from "express-session";
+import { __PROD__ } from "../constants";
 
 export const getSessionConfig = (): SessionOptions => {
   const WEEK_IN_MS = 1000 * 60 * 60 * 24 * 7;
 
   const sessionConfig: SessionOptions = {
+    name: "session",
     secret: "thisisnotagoodsecret!",
     resave: false,
     saveUninitialized: true,
     cookie: {
       httpOnly: true,
+      secure: __PROD__,
       expires: new Date(Date.now() + WEEK_IN_MS),
       maxAge: WEEK_IN_MS,
     },
